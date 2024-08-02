7b1f08a8d031528eb0b86b6c9304294dd33eda30
Pedro Noronha <pedro_noronha@outlook.com>
2024-08-02 09:31:15 -0300

    add cv

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -21,7 +21,7 @@ const Hero: React.FC<HeroProps> = ({ menuOpen }) => {
           </a>
           <a
             className={styles.contactBtn}
-            href="public/CV-Pedro Noronha _.pdf"
+            href="assets\cv\CV- Pedro Noronha _.pdf"
             target="_blank"
             rel="noopener noreferrer"
           >
